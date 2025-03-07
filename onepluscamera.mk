660ec84dc255ba3fa7617a84b61a334314e5185c
Terminator-J <terminator_j@hotbutteredmonkey.com>
2025-03-06 22:12:28 -0800

    opcam: Move configs to Android.bp modules

--- onepluscamera.mk
+++ onepluscamera.mk
@@ -13,13 +13,13 @@ PRODUCT_PACKAGES += \
     OnePlusGallery
 
 # Permissions/Sysconfig
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.oneplus.camera.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-com.oneplus.camera.xml \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.oneplus.camera.service.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.oneplus.camera.service.xml \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.oneplus.gallery.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-com.oneplus.gallery.xml \
-    $(LOCAL_PATH)/configs/permissions/default-permissions-oneplus.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/default-permissions-oneplus.xml \
-    $(LOCAL_PATH)/configs/sysconfig/hiddenapi-allowlist-oneplus.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-allowlist-oneplus.xml \
-    $(LOCAL_PATH)/configs/sysconfig/oneplus_google_lens_config.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/oneplus_google_lens_config.xml
+PRODUCT_PACKAGES += \
+    privapp-permissions-com.oneplus.camera.xml \
+    privapp-permissions-com.oneplus.camera.service.xml \
+    privapp-permissions-com.oneplus.gallery.xml \
+    default-permissions-oneplus.xml \
+    hiddenapi-allowlist-oneplus.xml \
+    oneplus_google_lens_config.xml
 
 # Properties
 ## See BoardConfigVendor.mk
