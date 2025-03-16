cb24ad67d78bcf19e881a7511fc3bbfbb53c80dc
Terminator-J <terminator_j@hotbutteredmonkey.com>
2025-03-15 17:26:03 -0700

    sepolicy: Add seapp context & policies for OnePlus Camera apps
    Based on pjgowtham's vendor opcam work
    - Replace 'oneplus' for 'oplus'
    - Includes fixes from "sepolicy: qti: Add rules for OPLUS Camera" from hardware/oplus/
    - Based on policy from OOS 11.1.2.2 for OP6-series
    - Use contexts from qcom's sepolicy_vndr/legacy-um/legacy/
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
    Co-authored-by: pjgowtham <pjgowtham@gmail.com>

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -4,3 +4,4 @@ TARGET_SYSTEM_PROP += vendor/oneplus/camera/system.prop
 TARGET_VENDOR_PROP += vendor/oneplus/camera/vendor.prop
 
 # SEPolicy
+include vendor/oneplus/camera/sepolicy/SEPolicy.mk
---
+++ sepolicy/SEPolicy.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2025 crDroid Android Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    vendor/oneplus/camera/sepolicy/vendor
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    vendor/oneplus/camera/sepolicy/private
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    vendor/oneplus/camera/sepolicy/public
